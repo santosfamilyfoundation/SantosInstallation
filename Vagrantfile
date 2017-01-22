622ebe529fa7a59682996412193a3c5876b40738
Philip Seger <seger.philip@gmail.com>
2017-01-22 13:05:51 -0500

    EDIT: one Vagrantfile for all, configured install, removed auth

---
+++ Vagrantfile
@@ -0,0 +1,51 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+require 'getoptlong'
+
+opts = GetoptLong.new(
+  ['--provider', GetoptLong::OPTIONAL_ARGUMENT],
+)
+
+provider = 'virtualbox'
+begin opts.each do |opt, arg|
+    case opt
+      when '--provider'
+        provider=arg
+    end # case
+  end # each
+  rescue
+end
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure("2") do |config|
+  config.vm.hostname = "vagrant"
+  config.vm.define "default-#{provider}"
+
+  config.vm.provider "virtualbox" do |vbox, override|
+    override.vm.box = "ubuntu/trusty64"
+    override.vm.box_check_update = false
+    override.vm.network "forwarded_port", guest: 8888, host: 8088
+    vbox.memory = "1024"
+  end
+
+  config.vm.provider :aws do |aws, override|
+    override.vm.box = "dummy"
+
+    # Need to add parameters
+    aws.access_key_id = ""
+    aws.secret_access_key = ""
+    aws.session_token = ""
+    aws.keypair_name = ""
+    aws.ami = ""
+    aws.region = ""
+    override.ssh.private_key_path = ""
+    
+    override.ssh.username = "ubuntu"
+  end
+
+  # Custom shell scripts
+  config.vm.provision :shell, path: "install.sh", privileged: false
+end
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository -y ppa:mc3man/trusty-media
 sudo echo "deb http://us.archive.ubuntu.com/ubuntu trusty main universe" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get -y install git libopencv-dev ffmpeg python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose python-shapely python-skimage build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev libtbb-dev libeigen3-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils libgtk2.0-dev libboost-all-dev doxygen unzip mercurial vim cmake-curses-gui sqlite3 sqlite3-doc libsqlite3-dev libcppunit-dev libcppunit-doc cmake-qt-gui python-sklearn
+sudo apt-get -y install git libopencv-dev ffmpeg python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose python-shapely python-skimage python-pip build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev libtbb-dev libeigen3-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils libgtk2.0-dev libboost-all-dev doxygen unzip mercurial vim cmake-curses-gui sqlite3 sqlite3-doc libsqlite3-dev libcppunit-dev libcppunit-doc cmake-qt-gui python-sklearn
 wget -O OpenCV-2.4.10.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.10/opencv-2.4.10.zip/download
 unzip OpenCV-2.4.10.zip
 rm OpenCV-2.4.10.zip
@@ -23,7 +23,7 @@ cd trajectorymanagementandanalysis/trunk/src/TrajectoryManagementAndAnalysis/
 cmake .
 make TrajectoryManagementAndAnalysis
 cd ~/Traffic/
-hg clone https://Nicolas@bitbucket.org/Nicolas/trafficintelligence
+hg clone https://bitbucket.org/santosfamilyfoundation/trafficintelligence
 cd trafficintelligence/c
 sed -i "3s,.*,TRAJECTORYMANAGEMENT_DIR=${HOME}/Traffic/trajectorymanagementandanalysis/trunk/src/TrajectoryManagementAndAnalysis," Makefile
 make feature-based-tracking
@@ -31,9 +31,12 @@ make tests
 cd ~/Traffic/trafficintelligence
 make doc
 sudo make install
-sudo echo "export PYTHONPATH="${PYTHONPATH}:/home/$USER/Traffic/trafficintelligence/python"" | sudo tee -a ~/.bashrc
-sudo echo "export PYTHONPATH="${PYTHONPATH}:/home/vagrant/TrafficCloud"" | sudo tee -a ~/.bashrc
+echo 'export PYTHONPATH="${PYTHONPATH}:/home/vagrant/Traffic/trafficintelligence/python"' | tee -a ~/.bashrc
+echo 'export PYTHONPATH="${PYTHONPATH}:/home/vagrant/SantosCloud"' | tee -a ~/.bashrc
 . ~/.bashrc
 sudo ln /dev/null /dev/raw1394
 cd
-git clone https://github.com/santosfamilyfoundation/TrafficCloud.git
+git clone https://github.com/santosfamilyfoundation/SantosCloud.git
+sudo pip install cvutils
+cd SantosCloud
+# sudo pip install -r requirements.txt
