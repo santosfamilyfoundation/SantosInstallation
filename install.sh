395408442f262df4a403b83902e4ee44832abe71
Jacob Riedel <jr112394@gmail.com>
2017-04-07 15:37:18 -0400

    Fix aws.sh, added gitignore

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 sudo add-apt-repository -y ppa:mc3man/trusty-media
 sudo echo "deb http://us.archive.ubuntu.com/ubuntu trusty main universe" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get -y install git libopencv-dev ffmpeg ipython ipython-notebook python-pandas python-sympy python-nose python-shapely python-skimage python-pip python-scipy build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev libtbb-dev libeigen3-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils libgtk2.0-dev libboost-all-dev doxygen unzip mercurial vim cmake-curses-gui sqlite3 sqlite3-doc libsqlite3-dev libcppunit-dev libcppunit-doc cmake-qt-gui python-sklearn
-curl -O -J -L http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.10/opencv-2.4.10.zip/download
-unzip opencv-2.4.10.zip
-rm opencv-2.4.10.zip
+sudo apt-get -y install git libopencv-dev ffmpeg python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose python-shapely python-skimage python-pip build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev libtbb-dev libeigen3-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils libgtk2.0-dev libboost-all-dev doxygen unzip mercurial vim cmake-curses-gui sqlite3 sqlite3-doc libsqlite3-dev libcppunit-dev libcppunit-doc cmake-qt-gui python-sklearn
+wget -O OpenCV-2.4.10.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.10/opencv-2.4.10.zip/download
+unzip OpenCV-2.4.10.zip
+rm OpenCV-2.4.10.zip
 cd opencv-2.4.10
 rm -rf build
 mkdir build
@@ -31,8 +31,8 @@ make tests
 cd ~/Traffic/trafficintelligence
 make doc
 sudo make install
-echo 'export PYTHONPATH="${PYTHONPATH}:/home/vagrant/Traffic/trafficintelligence/python"' | tee -a ~/.bashrc
-echo 'export PYTHONPATH="${PYTHONPATH}:/home/vagrant/SantosCloud"' | tee -a ~/.bashrc
+echo 'export PYTHONPATH=$PYTHONPATH:/home/vagrant/Traffic/trafficintelligence/python' | sudo tee -a ~/.bashrc
+sudo echo 'export PYTHONPATH=$PYTHONPATH:/home/vagrant/TrafficCloud' | sudo tee -a ~/.bashrc
 . ~/.bashrc
 sudo ln /dev/null /dev/raw1394
 cd
